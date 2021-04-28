15f862db9d742ea5314cbaf665d55d5a48124542
Adriano Ferreira <adriano.unicamp@gmail.com>
2021-04-28 13:54:24 -0300

    Creation of folders to organize every training round.

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ import os
 
 if __name__ == '__main__':
     prefix = '/home/adriano/Desktop/avaltech/ai/models/'
+    problem = 'regression'
+    body_part = 'garsup'
     current_day = datetime.now().date()
     current_time = datetime.now().strftime('%H:%M:%S')
-    folder_day = os.path.join(prefix, str(current_day))
+    problem_path = os.path.join(prefix, problem)
+    body_part_path = os.path.join(problem_path, body_part)
+    folder_day = os.path.join(body_part_path, str(current_day))
     folder_time = os.path.join(folder_day, current_time)
     path_exists = os.path.isdir(folder_time)
 
     if not path_exists:
         os.makedirs(folder_time)
 
-    cv.run_k_fold_cv(folder_time)
+    cv.run_k_fold_cv(folder_time, data_augmentation=False, conv_base='VGG16', body_part=body_part)
